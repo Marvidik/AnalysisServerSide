6a8ce76a00ec49e8958ef288329f10d1b761ad92
unknown <ebubeidika@gmail.com>
2024-01-22 00:26:11 +0100

    Added the Update API

--- google/urls.py
+++ google/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
-from .views import getlatestgoogle,post_new_analysis,get_particular_google
+from .views import getlatestgoogle,post_new_analysis,get_particular_google,update_todays_google
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("todays_data/",getlatestgoogle, name="todays_data"),
     path("addgoogle/",post_new_analysis,name="postgogle"),
-    path("filtergoogle/",get_particular_google,name="filter-google")
+    path("filtergoogle/",get_particular_google,name="filter-google"),
+    path("update",update_todays_google,name="update"),
 
    
     
--- google/views.py
+++ google/views.py
@@ -26,13 +26,22 @@ def getlatestgoogle(request):
      
 
 
-@api_view(["GET", "POST"])
+
+@api_view(["POST"])
 def post_new_analysis(request):
     if request.method == "POST":
         info = request.data
         serializer = GoogleDataSerializer(data=info)
 
         if serializer.is_valid():
+            # Extract the 'today' field from the request data
+            today = serializer.validated_data.get('today')
+
+            # Check if data with the same date already exists
+            if GoogleData.objects.filter(today=today).exists():
+                return Response({'message': 'Data with the same date already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Save the new data
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
@@ -53,3 +62,22 @@ def get_particular_google(request):
         return Response(serializer.data)
     else:
         return Response({'error': 'Missing or invalid "today" value in the request data'}, status=400)
+
+
+# For Updating Data For Google
+@api_view(["PUT"])
+def update_todays_google(request):
+    try:
+        google_data_instance = GoogleData.objects.get(today=date.today())  # Provide any conditions to get the specific instance
+    except GoogleData.DoesNotExist:
+        return Response({'message': 'GoogleData not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "PUT":
+        info = request.data
+        serializer = GoogleDataSerializer(google_data_instance, data=info)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
